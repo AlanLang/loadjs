9dd566a6867915dc7d10029b449b4512f33c11dd
alan <alan@fanruan.com>
2022-02-09 17:08:03 +0800

    add build action

--- src/loadjs.js
+++ src/loadjs.js
@@ -102,7 +102,7 @@ function loadFile(path, callbackFn, args, numTries) {
       async = args.async,
       maxTries = (args.numRetries || 0) + 1,
       beforeCallbackFn = args.before || devnull,
-      pathname = path.replace(/[\?|#].*$/, ''),
+      pathname = path,
       pathStripped = path.replace(/^(css|img)!/, ''),
       isLegacyIECss,
       e;
